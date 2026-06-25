7302ed451939bb49198ac6868147907766798508
Vinay-Partap <bhardwajvinay400@gmail.com>
2026-06-25 09:34:47 +0530

    Onboardings and rotes are added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const onboardingRoutes = require('./onboarding');
+const employeeRoutes = require('./employee');
+const reimbursementRoutes = require('./reimbursement');
 
 router.use('/onboardings', onboardingRoutes);
+router.use('/', employeeRoutes);
+router.use('/', reimbursementRoutes); // Mounts the reimbursement pipelines cleanly under the global /rest/ prefix
 
 module.exports = router;
